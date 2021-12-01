db257de6874377de48c7e089b9c2617068c98c86
Aziov Murodjon <murodjon.azimov.99@mail.ru>
2021-12-01 19:40:38 +0500

    fix: prepare my homework with an answer and fix it

--- Homework.js
+++ Homework.js
@@ -23,9 +23,9 @@
 
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
@@ -33,6 +33,12 @@ let personalMovieDB = {
   private: false
 };
 
-personalMovieDB.movies = prompt('Один из последних просмотренных фильмов?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d;
+      
 console.log(personalMovieDB);
